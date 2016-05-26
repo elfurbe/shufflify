9956597ff3baa080e31332c3562b99893c18f09b
Adam Michel <elfurbe@furbism.com>
2016-05-26 16:52:42 -0700

    Stubbed out all plugins. Started work on dymanic loader.

--- shufflify.py
+++ shufflify.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import sys,getopt
+import sys,getopt,os
+import collections
+from pprint import pprint
 
 def usage():
     print "usage: ",sys.argv[0]," [-hl] -m METHOD -i FILENAME [-o FILENAME]"
@@ -10,21 +12,47 @@ def usage():
     print "-i, --infile=FILE        input file of spotify URLs"
     print "-o, --outfile=FILE       output to specified file instead of stdout"
 
-def parse_plugins(plugindir):
-    print "Plugin directory:",plugindir
-    print "This will parse the plugins and build the plugin list"
+def loadImports(path):
+    files = os.listdir(path)
+    imps = []
 
-def list_plugins():
-    parse_plugins("plugins")
-    print "This will be a list of plugins"
+    for i in range(len(files)):
+        name = files[i].split('.')
+        if len(name) > 1:
+            if name[1] == 'py' and name[0] != '__init__':
+               name = name[0]
+               imps.append(name)
+
+    file = open(path+'__init__.py','w')
+
+    toWrite = '__all__ = '+str(imps)
+
+    file.write(toWrite)
+    file.close()
+
+def parse_plugins(plugindir):
+    loadImports('plugins/')
+    #from plugins import *
+    #plugins = ['furbify','qwijify','ogr','lcm','balanced']
+    #print "Plugin directory:",plugindir
+    #print "This will parse the plugins and build the plugin list"
+    return plugins
+
+def list_plugins(plugins):
+    #print "This will be a list of plugins"
+    pprint(plugins)
 
 def main(argv):
+    plugin_dir = "plugins"
+    plugins = parse_plugins(plugin_dir)
+
     try:
         opts, args = getopt.getopt(argv,"hlm:i:o:",["help","list","method=","infile=","outfile="])
-    except getopt.GetoptError:
-        print "Options not recognized."
+    except getopt.GetoptError, exc:
+        print exc.msg
         usage()
         sys.exit(2)
+
     method = ''
     infile = ''
     outfile = ''
@@ -33,7 +61,7 @@ def main(argv):
             usage()
             sys.exit(0)
         elif opt in ("-l","--list"):
-            list_plugins()
+            list_plugins(plugins)
             sys.exit(0)
         elif opt in ("-m","--method"):
             method = arg
@@ -41,8 +69,9 @@ def main(argv):
             infile = arg
         elif opt in ("-o","--outfile"):
             outfile = arg
+
     if not method or not infile:
-        print "Must specify a method and an input file"
+        print "Must include method and input file at minimum"
         usage()
         sys.exit(2)
 
